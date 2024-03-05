9ee418d4655f7f67395a83fe1b71876d532bd58b
Syed Umair <syed.yousuff@anywhere.co>
2024-03-05 08:02:07 +0530

    fix: update ReactDOM usage to use createRoot for concurrent mode rendering

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 
 function App() {
   return (
     <div>
-      <h1>Hello from React!</h1>
+          <h1>Hello from React!</h1>
+          <button onClick={() => { throw new Error('Sentry React Test Error') }}>Throw error</button>
     </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
